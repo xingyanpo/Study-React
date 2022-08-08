a222412cfbfc32a3d3a2e042896961ca7da165ae
jerry <909689902@qq.com>
2022-08-08 10:32:27 +0800

    Tom second change

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ import {createRoot} from 'react-dom/client'
 // import Slot from './components/插槽'
 // import App from './components/生命周期函数/运行时'
 // import App from './components/生命周期函数/运行时2'
-import App from './components/生命周期函数/销毁'
+import App from './components/生命周期函数/新生命周期函数案例-2'
 
 createRoot(document.getElementById('root')).render(<App/>)
