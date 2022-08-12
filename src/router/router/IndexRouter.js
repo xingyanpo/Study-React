f67c50e820b0ede5fd2ba4924ab2474d9685d04e
XYP <909689902@qq.com>
2022-08-12 08:03:13 +0800

    2022年8月12日08:03:03 first change

--- src/router/router/IndexRouter.js
+++ src/router/router/IndexRouter.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import {Route, HashRouter} from 'react-router-dom'
+import { Route, HashRouter, Switch, Redirect } from 'react-router-dom'
 
 import Main from '../view/Main'
 import List from '../view/List'
 import Me from '../view/Me'
+import NotFound from '../view/NotFound'
 
 export default function MyRouter() {
   return (
     <HashRouter>
-      <Route path='/main' component={Main}></Route>
-      <Route path='/list' component={List}></Route>
-      <Route path='/me' component={Me}></Route>
+      <Switch>
+        <Route path='/main' component={Main}></Route>
+        <Route path='/list' component={List}></Route>
+        <Route path='/me' component={Me}></Route>
+
+        <Redirect from='/' to='/main' exact></Redirect>
+
+        <Route component={NotFound}></Route>
+      </Switch>
     </HashRouter>
   )
 
--- src/router/view/Main.js
+++ src/router/view/Main.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Comingsoon from './main/Comingsoon'
+import Nowplaying from './main/Nowplaying'
+
+import {Route, Redirect, Switch} from 'react-router-dom'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>Main
+      <Switch>
+        <Route path='/main/nowplaying' component={Nowplaying}></Route>
+        <Route path='/main/comingsoon' component={Comingsoon}></Route>
+        <Redirect from='/main' to='/main/nowplaying'></Redirect>
+      </Switch>
+    </div>
   )
 }
