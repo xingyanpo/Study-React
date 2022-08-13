e90ecb8fba68558e4ae53260291b6b397dc46903
XYP <909689902@qq.com>
2022-08-13 23:37:53 +0800

    2022年8月13日23:37:49

--- src/router/view/Main.js
+++ src/router/view/Main.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Comingsoon from './main/Comingsoon'
 import Nowplaying from './main/Nowplaying'
 
-import {Route, Redirect, Switch} from 'react-router-dom'
+import {Route, Redirect, Switch, NavLink} from 'react-router-dom'
 
 export default function Main() {
   return (
     <div>Main
+      <div>
+        <NavLink activeClassName='tabbar-active' to='/main/nowplaying'>正在热映</NavLink>
+        <NavLink activeClassName='tabbar-active' to='/main/comingsoon'>即将上映</NavLink>
+      </div>
       <Switch>
         <Route path='/main/nowplaying' component={Nowplaying}></Route>
         <Route path='/main/comingsoon' component={Comingsoon}></Route>
