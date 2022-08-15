8ae85831bc800c0a73a0ec08201a3f74771eec81
jerry <909689902@qq.com>
2022-08-15 09:31:56 +0800

    2022年8月15日09:31:54

--- src/router/view/main/Comingsoon.js
+++ src/router/view/main/Comingsoon.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Comingsoon() {
+  const [list, setList] = useState([])
+  useEffect(() => {
+   axios.get('/api/mmdb/movie/v3/list/hot.json?ct=%E4%B8%8A%E6%B5%B7&ci=10&channelId=4').then(res => {
+    console.log(res)
+    setList(res.data.data.hot)
+   })
+  }, [])
   return (
-    <div>Comingsoon</div>
+    <div>
+      {
+        list.map((item, index) => 
+        <li key={item.id}>{item.nm}</li>
+        )
+      }
+    </div>
   )
 }
