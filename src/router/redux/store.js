cad562d18340d6d5ccde22b808738d814ed25d6b
XYP <909689902@qq.com>
2022-08-16 07:33:26 +0800

    2022年8月16日07:33:23

--- src/router/redux/store.js
+++ src/router/redux/store.js
@@ -1,7 +1,6 @@
 import {createStore} from 'redux'
 
 const reducer = (prevState={show: true}, action) => {
-  console.log(action)
   let newState = {...prevState}
   switch(action.type) {
     case 'hide':
