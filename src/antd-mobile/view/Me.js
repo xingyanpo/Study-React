c61bc1a6362883797a9012a1c8dc9accc3f6b05c
jerry <909689902@qq.com>
2022-08-22 17:32:23 +0800

    2022年8月22日17:32:21

--- src/antd-mobile/view/Me.js
+++ src/antd-mobile/view/Me.js
@@ -1,18 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import {withRouter} from 'react-router-dom'
-import axios from 'axios'
 
 function Me(props) {
-  useEffect(() => {
-    axios({
-      url:'/login/cellphone?phone=17634616135&password=xyp90925',
-      method: 'get'
-    }).then(res => {
-      console.log(res)
-    }).catch(res => {
-      console.log(res)
-    })
-  },[])
   return (
     <div>
       <button onClick={() => {
